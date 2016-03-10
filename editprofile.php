98c1c5023572ace7a9b36d30443c2670d52812ba
Lee Thomas <number8pie@gmail.com>
2016-03-10 10:04:01 +0000

    Tested profile picture upload and it didnt work. Removed isset from line 43 in editprofile.php, reverted back to code in book. Tested picture upload and it works but gives a notice: Notice: Undefined index: file in C:\xampp\htdocs\mismatch\editprofile.php on line 43 On line 43 of editprofile.php: Replaced		if ($_FILES['files']['error'] == 0) { With				if ($_FILES['new_picture']['error'] == 0) { Picture upload works fine and gives no notices.

--- editprofile.php
+++ editprofile.php
@@ -40,7 +40,7 @@
       if ((($new_picture_type == 'image/gif') || ($new_picture_type == 'image/jpeg') || ($new_picture_type == 'image/pjpeg') ||
         ($new_picture_type == 'image/png')) && ($new_picture_size > 0) && ($new_picture_size <= MM_MAXFILESIZE) &&
         ($new_picture_width <= MM_MAXIMGWIDTH) && ($new_picture_height <= MM_MAXIMGHEIGHT)) {
-        if (isset($_FILES['file']) && $_FILES['file']['error'] == 0) {
+        if ($_FILES['new_picture']['error'] == 0) {
           // Move the file to the target upload folder
           $target = MM_UPLOADPATH . basename($new_picture);
           if (move_uploaded_file($_FILES['new_picture']['tmp_name'], $target)) {
@@ -57,6 +57,7 @@
           }
         }
       }
+
       else {
         // The new picture file is not valid, so delete the temporary file and set the error flag
         @unlink($_FILES['new_picture']['tmp_name']);
